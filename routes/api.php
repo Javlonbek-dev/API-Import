0323fb2500cd8dcdb56df016b04fbd81fae93bb5
Javlonbek <javlonrahmatullayev07@gmail.com>
2024-06-08 19:22:14 +0500

    Delete Some files

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::apiResource('/user', UserController::class);
-Route::apiResource('/brand', BrandController::class);
-Route::apiResource('/branch', BranchController::class);
-Route::get('/brands/{region}/branches',[BrandController::class, 'branchesPerBrandInRegion']);
 
